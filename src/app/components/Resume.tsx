e28f5597bdd1bf9b7e7c7b18d17ce9bac68ba0a1
Dylan <34380246+DKotzer@users.noreply.github.com>
2024-11-17 17:49:37 -0500

    embeding resume fix pt2

--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -1,20 +1,57 @@
-const Resume: React.FC = () => {
+import { createTheme, darkScrollbar, ThemeProvider } from "@mui/material"
+
+interface ResumeProps {
+  darkMode?: boolean
+  handleThemeChange?: () => void
+}
+    
+const Resume: React.FC<ResumeProps> = ({
+  darkMode,
+  handleThemeChange,
+}) => {
+
+    const paletteType = darkMode ? "dark" : "light"
+
+    const theme = createTheme({
+      palette: {
+        mode: paletteType,
+        background: {
+          default: paletteType === "light" ? "#FFFFFF" : "#1e1e1e",
+        },
+      },
+      components: {
+        MuiCssBaseline: {
+          styleOverrides: {
+            body: paletteType === "dark" ? darkScrollbar() : null,
+          },
+        },
+        MuiDivider: {
+          styleOverrides: {
+            root: {
+              borderColor: "rgba(255, 255, 255, 0.12)",
+            },
+          },
+        },
+      },
+    })
   return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <object
-        data='/dylan_kotzer.pdf'
-        type='application/pdf'
-        width='100%'
-        height='100%'
-        aria-labelledby='PDF document'
-        title='Embedded PDF Viewer'
-      >
-        <p>
-          Your browser does not support PDFs.
-          <a href='/dylan_kotzer.pdf'>Download the PDF</a>
-        </p>
-      </object>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='do-not-overlap' style={{ width: "100%", height: "100%" }}>
+        <object
+          data='/dylan_kotzer.pdf'
+          type='application/pdf'
+          width='100%'
+          height='100%'
+          aria-labelledby='PDF document'
+          title='Embedded PDF Viewer'
+        >
+          <p>
+            Your browser does not support PDFs.
+            <a href='/dylan_kotzer.pdf'>Download the PDF</a>
+          </p>
+        </object>
+      </div>
+    </ThemeProvider>
   )
 }
 
